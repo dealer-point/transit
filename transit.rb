e8d1401869c5ea62c8e9e24de90e80bf531a0df1
Vladimir Trubachoff <trubachoff@gmail.com>
2016-07-15 12:02:41 +0300

    Add param -c <file.yaml> for change config file

--- transit.rb
+++ transit.rb
@@ -2,7 +2,22 @@
 
 require "yaml"
 
-CONFIG = YAML.load_file("config.yml")
+config_file = ".transit.yaml"
+
+puts "Tranist search russian letters in files"
+
+if ARGV[0] == "-c"
+  if ARGV[1] && File.file?(ARGV[1])
+    config_file = ARGV[1]
+  else
+    puts "Usage:"
+    puts "#{__FILE__} -c <config name>.yaml"
+
+    exit
+  end
+end
+
+CONFIG = YAML.load_file(config_file)
 
 paths = CONFIG['include']
 exclude = CONFIG['exclude']
@@ -20,30 +35,37 @@ def scan_file(path)
     end
   rescue => err
     puts "Exception: #{err}"
-    #err
+    err
   end
 end
 
 def rscan_dir(root_path, exclude)
   if File.directory?(root_path)
     #puts root_path
+
     Dir.foreach root_path do |path|
       unless path == '.' || path == '..'
         next_path = "#{root_path}/#{path}"
+
         if File.directory?(next_path)
           rscan_dir(next_path, exclude) unless path == '.' || path == '..'
         else
+
           fl_exclude = false
+
           exclude.each do |regs|
             if File.fnmatch("**/#{regs}", next_path, File::FNM_PATHNAME | File::FNM_DOTMATCH)
               fl_exclude = true
+
               break
             end
           end
+
           unless fl_exclude
             puts "File: #{path}\n"
             scan_file next_path
           end
+
         end
       end
     end
